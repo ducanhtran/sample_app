d8119e0097873783118962e72363fb2fb760f9f5
Tran Duc Anh <tran.duc.anh@framgia.com>
2017-12-25 18:28:14 +0700

    Chapter 8: Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = t"home.welcome"
       redirect_to @user
     else
@@ -19,17 +20,10 @@ class UsersController < ApplicationController
   end
 
   private
-<<<<<<< 1b3dea2e64ca7faded8ce980f62784f40469d340
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
         :password_confirmation)
     end
 
-=======
-  def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                :password_confirmation)
-  end
->>>>>>> Chapter 7: Finish user signup
 end
